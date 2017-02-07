88a66481072bb88be34eac247e066f6fcd2e8fa5
Amanda C <amandan.chavez@gmail.com>
2017-02-07 16:51:21 -0700

    add bookmark container shell

--- javascript.js
+++ javascript.js
@@ -1,15 +1,31 @@
+var $webTitle = '';
+
 $('.create').on('click', function() {
   console.log('A click!')
-  var $userInput = $('.title').val();
-  console.log($userInput);
-  $('.bookmark-container').append("random text "+$userInput);
+  $webTitle = $('.title').val();
+  console.log($webTitle);
+  $('.bookmark-container').append(
+  '<section class="bookmark">'+
+  '<h2>'+$webTitle+'</h2>'+
+  '<hr>'+
+  '<h3>URL</h3>'+
+  '<hr>'+
+  '<button class="read">Read</button>'+
+  '<button class="delete">Delete</button>'+
+  '</section>');
+  //$('.bookmark-container').append(  '<section class="bookmark">');
+  //$('.bookmark-container').append(  '<h2>Bookmark Title</h2>');
+  //$('.bookmark-container').append(  '</section>');
 
-  $('.bookmark-container').append('<button>Read</button>');
+
+
 })
 
 $('.bookmark-container').on('click', 'button', function(event){
   console.log(event.target);
-  console.log('something worked');
+  console.log($webTitle);
+  $(this).toggleClass('red');
+  $('')
 })
 
 
