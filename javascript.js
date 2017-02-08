83d609dc025fd0e24cfc42ad08968997b7ef7084
Amanda C <amandan.chavez@gmail.com>
2017-02-07 18:03:40 -0700

    add phase two items

--- javascript.js
+++ javascript.js
@@ -1,35 +1,43 @@
 var $webTitle = '';
+var $webURL = '';
 
 $('.create').on('click', function() {
   console.log('A click!')
+  if ($('.title').val() == '' || $('.url').val() == ''){
+    alert('Error: Enter a Valid Input');
+    return; 
+  }
   $webTitle = $('.title').val();
+  $webURL = $('.url').val();
   console.log($webTitle);
-  $('.bookmark-container').append(
+  $('.bookmark-container').prepend(
   '<section class="bookmark">'+
   '<h2>'+$webTitle+'</h2>'+
   '<hr>'+
-  '<h3>URL</h3>'+
+  '<h3>'+$webURL+'</h3>'+
   '<hr>'+
   '<button class="read">Read</button>'+
   '<button class="delete">Delete</button>'+
   '</section>');
-  //$('.bookmark-container').append(  '<section class="bookmark">');
-  //$('.bookmark-container').append(  '<h2>Bookmark Title</h2>');
-  //$('.bookmark-container').append(  '</section>');
-
-
 
 })
+
 
 $('.bookmark-container').on('click', 'button', function(event){
   console.log(event.target);
   console.log($webTitle);
   $(this).toggleClass('red');
-  $('')
 })
 
+$('.bookmark-container').on('click', 'button', function(event){
+  console.log(event.target);
+  console.log($webURL);
+  $(this).parent().remove();
+})
 
-
-// readbtn.on('click', function() {
-//   $('.bookmark-container').append('class ="read"');
+// $('.title').(function() {
+//   if ($('.title').val() == '' || $('.url').val() == ''){
+//     alert('Error: Enter a Title');
+//   }
+//
 // })
